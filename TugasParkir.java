c858868b5489b9cc3556307d628978f4d6a42628
Muhamad Armanditto <deepfreeze1197@gmail.com>
2020-01-06 15:13:49 +0700

    TugasParkir.java (Dry Version)

--- TugasParkir.java
+++ TugasParkir.java
@@ -5,8 +5,8 @@ public class TugasParkir {
         int lamaparkir;
         int biayaparkir=0;
         int biayaparkirnext=0;
-        String tipe;
-        int total;
+        String tipe="";
+        int total=0;
 
         for (int i = 0; i < 2; i++){
             Scanner Obj=new Scanner(System.in);
@@ -24,34 +24,36 @@ public class TugasParkir {
             if(jmasuk>jkeluar){
                 jkeluar=jkeluar+24;
             }
-        
-            System.out.println();
-            System.out.println("Plat: " +plat);
             
             if(jenis==1){
                 tipe="Mobil";
                 biayaparkir = 5000;
                 biayaparkirnext = 3000;
-                System.out.println("Jenis Kendaraan: "+tipe);
             }else if(jenis==2){
                 tipe="Motor";
                 biayaparkir = 3000;
                 biayaparkirnext = 1500;
-                System.out.println("Jenis Kendaraan: "+tipe);
             }else{
                 System.out.println("Jenis Kendaraan Tidak Terdaftar");
             }
 
             lamaparkir=jkeluar-jmasuk;
-            System.out.println("Lama Parkir: "+lamaparkir+" Jam");
             if(lamaparkir==1){
                 total = biayaparkir;
-                System.out.println("Biaya Parkir: "+total);
             }
             else if(lamaparkir>1){
-                total = biayaparkir+((lamaparkir-1)*biayaparkirnext);
-                System.out.println("Biaya Parkir: "+total);
+                total = biayaparkir+((lamaparkir-1)*biayaparkirnext);    
+            }else if(lamaparkir==0){
+                lamaparkir = 1;
+                total = biayaparkir;
             }
+
+            System.out.println();
+            System.out.println("Plat            : " +plat);
+            System.out.println("Jenis Kendaraan : "+tipe);
+            System.out.println("Lama Parkir     : "+lamaparkir+" Jam");
+            System.out.println("Biaya Parkir    : "+total);
+            System.out.println();
         }
     }
 }
